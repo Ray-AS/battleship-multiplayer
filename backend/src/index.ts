a6f126c5879ce7bd051829856c888fd2b4ffc74f
Ray-AS <abisuthans.business@gmail.com>
2026-01-21 23:16:54 -0500

    add cors to allow requests from frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,15 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors';
 import swaggerPlugin from "./plugins/swagger.ts";
 import { gameRoutes } from "./routes/gameRoutes.ts";
 import { DEFAULT_BOARD_SIZE, SHIPS } from "./configs.ts";
 
 const fastify = Fastify({ logger: true });
+await fastify.register(cors, { 
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'] 
+});
+
 export const PORT = 3000;
 await fastify.register(swaggerPlugin);
 
